792dfb6db24b9de67244c82687abaf7ec368c02c
neerojstha <nirojstha@hotmail.com>
2023-02-27 12:59:52 +0000

    rendered user information

--- assets/js/git-hub information.js
+++ assets/js/git-hub information.js
@@ -1,3 +1,19 @@
+function userInformationHTML(user) {
+    return`
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="#gh-content">
+            <div class="#gh-avatar">
+                <a href="${username.html_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.publin_repos}</p>
+        </div>`;
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
