63d19d75ee02a1a13e83e2549afc8e3bebd9d2ea
neerojstha <nirojstha@hotmail.com>
2023-02-27 13:37:07 +0000

    error corrected

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var map = new google.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
             lat: 46.619261,
@@ -10,10 +10,11 @@ function initMap() {
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        {lat: 40.785091, lng: -73.968285},
-        {lat: 41.084045, lng: -73.874245},
-        {lat: 40.754932, lng: -73.984016}
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
     ];
+
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
@@ -21,6 +22,5 @@ function initMap() {
         });
     });
 
-    var markerCluster = new Markercluster(map, markers, { imagepath: 'https://developers.google.com/maps/documentation/javascript/examples/markercluster/m'});
-
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
